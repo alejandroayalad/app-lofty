ad9e24f56911cf733ddbb287fa6f65623c7c80dd
alejandroayalad <alx.ayala08@gmail.com>
2024-12-03 20:05:33 -0600

    cambio en puerto

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const PORT = 4010;
-
+const PORT = process.env.PORT || 4010;
 
 app.use((_req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   next();
 });
 
-
 app.get('/api/pets', (_req, res) => {
   fs.readFile('./data.json', 'utf-8', (err, data) => {
-    if (err) return res.status(500).json({ error: "Error loading data" });
-    res.json(JSON.parse(data)); // Devolver el JSON parseado
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Error loading data' });
+    }
+    res.json(JSON.parse(data));
   });
 });
-
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
